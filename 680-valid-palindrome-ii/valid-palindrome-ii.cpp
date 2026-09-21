07ea040654989ab90fb30515f7fc13c22463c9be
Alexander Petrov <112880427+alexandernotalex@users.noreply.github.com>
2026-09-21 12:06:59 +0300

    Time: 12 ms (6.92%) | Memory: 26.8 MB (14.10%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -14,12 +14,7 @@ public:
         int begin = 0, end = s.length() - 1;
         while (begin < end) {
             if (s[begin] != s[end]) {
-                bool flag_1 = IsPalindrome(begin + 1, end, s);
-                bool flag_2 = IsPalindrome(begin, end - 1, s);
-                if (flag_1 || flag_2) {
-                    return true;
-                }
-                return false;
+                return IsPalindrome(begin + 1, end, s) || IsPalindrome(begin, end - 1, s);
             }
             ++begin;
             --end;
