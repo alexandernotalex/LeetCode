e38491d4b6af259dd0ac7b292714ce64f521bc51
Alexander Petrov <112880427+alexandernotalex@users.noreply.github.com>
2026-09-24 14:56:50 +0300

    Time: 12 ms (87.23%) | Memory: 12.3 MB (54.11%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseBits(self, n):
+        ans = 0
+        bit = 0
+        counter = 0
+        while n:
+            ans = ans * 2 + (n & 1)
+            n = n >> 1
+            counter += 1
+        while counter < 32:
+            ans *= 2
+            counter += 1
+        return ans
+
+
+        
